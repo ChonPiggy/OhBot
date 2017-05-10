8bbc835e2120beb91f63c37f2f4cf364950d79fb
Piggy <piggylee@nforetek.com>
2017-05-10 19:17:51 +0800

    Add AUD dollors feature name in list.

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -1593,7 +1593,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
 
                 }
                 if(text.equals("")){
-                    strResult = "義大利?維大力? \n請輸入 這些幣別：\n美金 日圓 人民幣 歐元 \n港幣 英鎊 韓元 越南盾";
+                    strResult = "義大利?維大力? \n請輸入 這些幣別：\n美金 日圓 人民幣 歐元 \n港幣 英鎊 韓元 越南盾 澳幣";
                     this.replyText(replyToken, strResult);
                 } else if (text.equals("TWD")){
                     this.replyText(replyToken, "現鈔賣出去巷口便利商店");
