a9c34c7831c6911f004768a84be122ece78d2be3
Piggy <piggylee@nforetek.com>
2017-09-22 02:45:39 +0800

    IDR directly change 2

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -1696,7 +1696,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
                     return;
                 }
         }catch (IOException e2) {
-            return e2;
+            throw e2;
         }
 
 
