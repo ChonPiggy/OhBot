18728c8e6db4ab9db7fb1a5721bcaa871175cd23
Piggy <piggylee@nforetek.com>
2018-01-15 13:57:08 +0800

    Implement random girl fetch from jandan. 13

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -2271,6 +2271,14 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             String url="http://jandan.net/ooxx";
             log.info(url);
             HttpGet httpGet = new HttpGet(url);
+            httpGet.addHeader( "User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36" );
+            httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484" );
+            try {
+                // 不敢爬太快 
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
             CloseableHttpResponse response = httpClient.execute(httpGet);
             log.info(String.valueOf(response.getStatusLine().getStatusCode()));
             HttpEntity httpEntity = response.getEntity();
@@ -2314,8 +2322,8 @@ This code is public domain: you are free to use, link and/or modify it in any wa
                     String html = Utils.convertStreamToString(in);
                      // 網頁內容解析
                     new Thread( new JianDanHtmlParser(html, i)).start();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                } catch (Exception e1) {
+                    e1.printStackTrace();
                 }
             }
 
