dc9af0a55e83d26ebff5f2180b4011f00d2365f4
Piggy <piggylee@nforetek.com>
2018-01-15 14:51:33 +0800

    Implement random girl fetch from jandan. 18

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -2286,11 +2286,20 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             String maxPage = "";
             int mMaxPageInt = 0;
 
+            String jsPath = "";
+
             maxPage = EntityUtils.toString(httpEntity, "utf-8");
+            jsPath = maxPage;
+
             maxPage = maxPage.substring(maxPage.indexOf("current-comment-page\">[")+23, maxPage.length());
             maxPage = maxPage.substring(0, maxPage.indexOf("]<"));
+
+            jsPath = jsPath.substring(jsPath.indexOf("<script src=\"//cdn.jandan.net/static/min/")+13, jsPath.length());
+            jsPath = jsPath.substring(0, jsPath.indexOf("\"></script>"));
+
             
             log.info("Piggy Check max page string: " + maxPage);
+            log.info("Piggy Check js path: " + jsPath);
 
             try {
                 mMaxPageInt = Integer.parseInt(maxPage);
@@ -2301,29 +2310,29 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             }
 
             log.info("Piggy Check max page int: " + mMaxPageInt);
+            
 
-
-            RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD). setConnectionRequestTimeout(6000).setConnectTimeout(6000 ).build();
-            httpClient = HttpClients.custom().setDefaultRequestConfig(globalConfig).build();
-            log.info("1秒後開始抓取煎蛋妹子圖...");
-            for ( int i = mMaxPageInt; i > 0; i--) {
-                // 創建一個GET請求 
-                httpGet = new HttpGet( "http://jandan.net/ooxx/page-" + i);
-                httpGet.addHeader( "User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36" );
-                httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484" );
-                try {
-                    // 不敢爬太快 
-                    Thread. sleep(1000);
-                    // 發送請求，並執行 
-                    response = httpClient.execute(httpGet);
-                    InputStream in = response.getEntity().getContent();
-                    String html = Utils.convertStreamToString(in);
-                     // 網頁內容解析
-                    new Thread( new JianDanHtmlParser(html, i)).start();
-                } catch (Exception e1) {
-                    e1.printStackTrace();
-                }
-            }
+            // RequestConfig globalConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD). setConnectionRequestTimeout(6000).setConnectTimeout(6000 ).build();
+            // httpClient = HttpClients.custom().setDefaultRequestConfig(globalConfig).build();
+            // log.info("1秒後開始抓取煎蛋妹子圖...");
+            // for ( int i = mMaxPageInt; i > 0; i--) {
+            //     // 創建一個GET請求 
+            //     httpGet = new HttpGet( "http://jandan.net/ooxx/page-" + i);
+            //     httpGet.addHeader( "User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36" );
+            //     httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484" );
+            //     try {
+            //         // 不敢爬太快 
+            //         Thread. sleep(1000);
+            //         // 發送請求，並執行 
+            //         response = httpClient.execute(httpGet);
+            //         InputStream in = response.getEntity().getContent();
+            //         String html = Utils.convertStreamToString(in);
+            //          // 網頁內容解析
+            //         new Thread( new JianDanHtmlParser(html, i, jsPath)).start();
+            //     } catch (Exception e1) {
+            //         e1.printStackTrace();
+            //     }
+            // }
 
 
         }catch (Exception e2) {
@@ -2333,10 +2342,12 @@ This code is public domain: you are free to use, link and/or modify it in any wa
 
     public class JianDanHtmlParser implements Runnable {
         private String html;
+        private String jsPath;
         private int page;
-        public JianDanHtmlParser(String html, int page) {
+        public JianDanHtmlParser(String html, int page, String js) {
         this.html = html;
         this.page = page;
+        this.jsPath = js;
     }
     @Override
     public  void run() {
