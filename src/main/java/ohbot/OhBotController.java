b94a9ec7eac7245e20ab97ae100d7c4fff82d39b
Piggy <piggylee@nforetek.com>
2018-01-25 18:56:41 +0800

    Implement random girl fetch from jandan. js part 17

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -2390,7 +2390,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             log.info("Piggy Check matcher.group(): " + result);
 
             result = result.substring(result.indexOf("class=\"img-hash\">")+15, result.length());
-            result = result.substring(0, result.indexOf("</span> "));
+            result = result.substring(0, result.indexOf("</span>"));
 
             log.info("Piggy Check result: " + result);
 
