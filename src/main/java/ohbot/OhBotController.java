44810f6ad2a90ad47a2a89e9cfd47a776458f0c1
Piggy <piggylee@nforetek.com>
2018-01-26 14:03:02 +0800

    Implement Pexel random image 5

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -89,6 +89,17 @@ public class OhBotController {
     private List<String> mPexelGirlList = new ArrayList<String> ();
     private List<String> mPexelManList = new ArrayList<String> ();
     private List<String> mPexelWomenList = new ArrayList<String> ();
+    private List<String> mUserAgentList = new ArraysList<String> (
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:23.0) Gecko/20130406 Firefox/23.0",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64; rv:18.0) Gecko/20100101 Firefox/18.0",
+        "Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533+ \(KHTML, like Gecko) Element Browser 5.0",
+        "IBM WebExplorer /v0.94', 'Galaxy/1.0 [en] (Mac OS X 10.5.6; U; en)",
+        "Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.1; WOW64; Trident/6.0)",
+        "Opera/9.80 (Windows NT 6.0) Presto/2.12.388 Version/12.14",
+        "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) \Version/6.0 Mobile/10A5355d Safari/8536.25",
+        "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) \Chrome/28.0.1468.0 Safari/537.36",
+        "Mozilla/5.0 (compatible; MSIE 9.0; Windows NT 6.0; Trident/5.0; TheWorld)",
+        "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36");
 
     @Autowired
     private LineMessagingService lineMessagingService;
@@ -2545,12 +2556,19 @@ This code is public domain: you are free to use, link and/or modify it in any wa
 
     private String getRandomPexelsImageUrl(String target) {
         try {
+
+            Random randomGenerator = new Random();
+            int random_num = randomGenerator.nextInt(maxPageInt);
+
+
+
             CloseableHttpClient httpClient = HttpClients.createDefault();
             String url="https://www.pexels.com/search/" + target;
             log.info("getRandomPexelsImageUrl:" + url);
             HttpGet httpGet = new HttpGet(url);
-            httpGet.addHeader( "User-Agent","Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.152 Safari/537.36" );
+            httpGet.addHeader("User-Agent",mUserAgentList.get(random_num));
             httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484" );
+
 
             CloseableHttpResponse response = httpClient.execute(httpGet);
             log.info(String.valueOf(response.getStatusLine().getStatusCode()));
@@ -2580,8 +2598,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             }
             log.info("Piggy Check maxPageInt: " + maxPageInt);
 
-            Random randomGenerator = new Random();
-            int random_num = randomGenerator.nextInt(maxPageInt);
+            random_num = randomGenerator.nextInt(maxPageInt);
 
             log.info("Piggy Check random_num: " + random_num);
 
