2dad2c382fd13e42156535437545ea6f5339475d
Piggy <piggylee@nforetek.com>
2019-01-06 01:18:10 +0800

    Add Amazon jp search ImageCarouselColumn 04

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -2,7 +2,7 @@ package ohbot;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.linecorp.bot.client.lineMessagingClient;
+import com.linecorp.bot.client.LineMessagingClient;
 import com.linecorp.bot.model.PushMessage;
 import com.linecorp.bot.model.ReplyMessage;
 import com.linecorp.bot.model.action.URIAction;
