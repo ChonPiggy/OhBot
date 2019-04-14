c61769c7c153c212531feb669ae8236eb344d275
ChonPiggy <ChonPiggy@ChonPiggys-iMac.local>
2019-04-14 12:56:30 +0800

    Try Lin Ho image 11

--- src/main/java/ohbot/LinHoImageHelper.java
+++ src/main/java/ohbot/LinHoImageHelper.java
@@ -15,28 +15,28 @@ import org.apache.http.impl.client.HttpClientBuilder;
 public class LinHoImageHelper {
     private static final String generateUrl = "https://singengo.com/api/v1/generate";
 
-    public static HttpResponse getImageUrl(String message) {
-        String result = "";
-        HttpResponse response = null;
-        try {
-            HttpClient httpClient = HttpClientBuilder.create().build();
-            HttpPost request = new HttpPost(generateUrl);
-            request.addHeader("content-type", "application/x-www-form-urlencoded");
-            request.setHeader("User-Agent",
-                                  "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
+    // public static HttpResponse getImageUrl(String message) {
+    //     String result = "";
+    //     HttpResponse response = null;
+    //     try {
+    //         HttpClient httpClient = HttpClientBuilder.create().build();
+    //         HttpPost request = new HttpPost(generateUrl);
+    //         request.addHeader("content-type", "application/x-www-form-urlencoded");
+    //         request.setHeader("User-Agent",
+    //                               "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");
 
-            request.setHeader("Connection","keep-alive");
-            StringEntity params =new StringEntity("txt=%E7%89%B9%E5%83%B9&type=1&twid=");
-            request.setEntity(params);
-            response = httpClient.execute(request);
-            // log.info("Piggy Check response: " + response);
-            // log.info(String.valueOf(response.getStatusLine().getStatusCode()));
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+    //         request.setHeader("Connection","keep-alive");
+    //         StringEntity params =new StringEntity("txt=%E7%89%B9%E5%83%B9&type=1&twid=");
+    //         request.setEntity(params);
+    //         response = httpClient.execute(request);
+    //         // log.info("Piggy Check response: " + response);
+    //         // log.info(String.valueOf(response.getStatusLine().getStatusCode()));
+    //     } catch (Exception e) {
+    //         e.printStackTrace();
+    //     }
 
-        return response;
-    }
+    //     return response;
+    // }
 
     public static String getImageUrl(String message) {
         String result = "";
