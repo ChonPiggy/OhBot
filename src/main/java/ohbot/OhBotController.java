12c10e2285cf2d0ff06d2bf752afec7e266d85d0
ChonPiggy <ChonPiggy@ChonPiggys-iMac.local>
2019-05-30 08:27:55 +0800

    Try to fix process random get image 03

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -3934,7 +3934,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
                 }
             }
             else {
-                Pattern patternJp = Pattern.compile("(http|ftp|https)://([\w_-]+(?:(?:\.[\w_-]+)+))([\w.,@?^=%&:/~+#-]*[\w@?^=%&/~+#-])?.(jpeg|jpg)");
+                Pattern patternJp = Pattern.compile("(http|ftp|https):\\/\\/([\\w_-]+(?:(?:\\.[\\w_-]+)+))([\\w.,@?^=%&:\\/~+#-]*[\\w@?^=%&\\/~+#-])?.(jpeg|jpg)");
                 Matcher matcherJp = patternJp.matcher(result_image_image);
                 while(matcherJp.find()){
                     String result = matcherJp.group();
