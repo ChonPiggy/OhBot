9e3fd410bfb932492b134b34529ae0c0fc71dbc3
Piggy <piggylee@nforetek.com>
2020-02-21 20:03:00 +0800

    Corona virus wiki rank crawl 12

--- src/main/java/ohbot/CoronaVirusWikiRankCrawlThread.java
+++ src/main/java/ohbot/CoronaVirusWikiRankCrawlThread.java
@@ -182,7 +182,7 @@ public class CoronaVirusWikiRankCrawlThread extends Thread {
             result = EmojiUtils.emojify(":warning:") + "中國肺炎全球傷亡人數" + EmojiUtils.emojify(":warning:") + "\n" + mUpdateTime + "\n";
             for (CoronaVirusInfo info : mCVIList) {
                 if (info.getCountry().equals("臺灣")) {
-                    result += EmojiUtils.emojify("taiwan")
+                    result += EmojiUtils.emojify("taiwan");
                 }
                 result += (info + "\n");
             }
