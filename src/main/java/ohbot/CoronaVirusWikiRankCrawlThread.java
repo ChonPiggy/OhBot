3e5d7ac0b43b4a749bd63db0e79a0ca81ca5a293
Piggy <piggylee@nforetek.com>
2020-02-23 14:48:22 +0800

    Corona virus wiki rank crawl 21

--- src/main/java/ohbot/CoronaVirusWikiRankCrawlThread.java
+++ src/main/java/ohbot/CoronaVirusWikiRankCrawlThread.java
@@ -126,7 +126,7 @@ public class CoronaVirusWikiRankCrawlThread extends Thread {
 
             // Catch update time
             String temp = strResult.substring(strResult.indexOf("截至"), strResult.length());
-            mUpdateTime = temp.substring(0, temp.indexOf("<span"));
+            mUpdateTime = temp.substring(0, temp.indexOf("日")+1);
 
             while (strResult.contains("<td><span class=\"flagicon\">")) {
                 String country = "";
