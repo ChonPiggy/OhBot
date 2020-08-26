58ebc62659522fc11165c125b6da1212f09d36b2
Piggy <piggylee@nforetek.com>
2020-08-26 18:17:54 +0800

    Try ImageCarouselColumn 04

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -67,7 +67,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import retrofit2.Response;
+
 
 import javax.net.ssl.*;
 import java.io.*;
