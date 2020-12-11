fac31ba71f61cbf929458fa7a1a8a5fa8bcc030d
Piggy <piggylee@nforetek.com>
2020-12-11 11:41:26 +0800

    Add announcement feature 04

---
+++ src/main/java/ohbot/AnnouncementManager.java
@@ -0,0 +1,71 @@
+package ohbot;
+
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.TimeZone;
+
+
+public class AnnouncementManager {
+	
+	private static String sAnnounceMessage = "";
+	private static long mStartTime;
+	
+	private static class AnnounceGroup {
+		private String groupId = "";
+		private String announcedMessage = "";
+		private boolean isAnnounced = false;
+		private boolean isAnnounceImage = false;
+		
+		public AnnounceGroup(String group, String message) {
+			groupId = group;
+			if (message != null) {
+				if (message.startsWith("http") && 
+						(message.endsWith(".jpg") 
+								|| message.endsWith(".jpeg") 
+								|| message.endsWith(".png"))) {
+					isAnnounceImage = true;
+				}
+				announcedMessage = message;
+			}
+			announcedMessage = "N/A";
+		}
+	}
+	
+	private static HashMap<String, AnnounceGroup> sAnnouncedGroups = new HashMap<>();
+	private static Calendar announceExpiredTime;
+	
+	// expired time with minute
+	public static void announceNewMessage(String message, boolean isNeedExpire, int expiredTime) {
+		sAnnouncedGroups.clear();
+		sAnnounceMessage = message;
+		if (isNeedExpire) {
+			announceExpiredTime = Calendar.getInstance(TimeZone.getDefault());
+			announceExpiredTime.setTimeInMillis(System.currentTimeMillis()+((expiredTime)*60*1000));
+		}
+	}
+	
+	public static String removeAnnouncement() {
+		sAnnouncedGroups.clear();
+		String message = sAnnounceMessage;
+		sAnnounceMessage = null;
+		return message;
+	}
+	
+	public static String processAnnounceMessage(String groupId) {
+		if (sAnnounceMessage == null) {
+			return null;
+		}
+		if (!sAnnouncedGroups.containsKey(groupId)) {
+			// Need announce
+			Calendar current = Calendar.getInstance(TimeZone.getDefault());
+            current.setTimeInMillis(System.currentTimeMillis());
+			if (current.getTimeInMillis() - announceExpiredTime.getTimeInMillis() < 0) {
+				// not expired, need announce if needed.
+				sAnnouncedGroups.put(groupId, new AnnounceGroup(groupId, sAnnounceMessage));
+				return sAnnounceMessage;
+			}
+		}
+		return null;
+	}
+
+}
