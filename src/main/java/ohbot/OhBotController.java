ba92a13fcb42fc16a38e6e387c5aa83c91ac02c3
Piggy <piggylee@nforetek.com>
2021-04-28 16:50:40 +0800

    Try to fix stock issue 05

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -3173,7 +3173,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
                               "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36");*/
             httpget.setHeader("User-Agent",
                               "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.85 Safari/537.36");
-            CloseableHttpResponse response = httpClient.execute(httpGet);
+            CloseableHttpResponse response = httpClient.execute(httpget);
             //CloseableHttpResponse response = defaultHttpClient.execute(httpget);
             //PgLog.info(String.valueOf(response.getStatusLine().getStatusCode()));
             HttpEntity httpEntity = response.getEntity();
