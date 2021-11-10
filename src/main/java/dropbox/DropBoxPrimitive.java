b2e7492cd6709fb706a826eee9f4f641e4125aff
Piggy <piggylee@nforetek.com>
2021-11-10 16:21:59 +0800

    Try to upload to DropBox 06

--- src/main/java/dropbox/DropBoxPrimitive.java
+++ src/main/java/dropbox/DropBoxPrimitive.java
@@ -83,7 +83,7 @@ public class DropBoxPrimitive {
 		// Upload "test.txt" to Dropbox
 		try (InputStream in = new FileInputStream(file)) {
 			try {
-				FileMetadata metadata = client.files().uploadBuilder("/test.txt")
+				FileMetadata metadata = client.files().uploadBuilder(file.getName())
 						.uploadAndFinish(in);
 			} catch (DbxException e) {
 				// TODO Auto-generated catch block
