ffe2bbbf62e5a18f929c97f5e8ddb5d629d765be
Piggy <piggylee@nforetek.com>
2023-06-07 20:59:13 +0800

    Modify get image list policy 08

--- src/main/java/ohbot/OhBotController.java
+++ src/main/java/ohbot/OhBotController.java
@@ -5338,9 +5338,9 @@ This code is public domain: you are free to use, link and/or modify it in any wa
         if (!text.startsWith("http")) {
             return;
         }
-        if (text.contains("www.ptt.cc")) {
+        /*if (text.contains("www.ptt.cc")) {
             sendPttOver18Checker();
-        }
+        }*/
 
         try{
 
@@ -5352,7 +5352,7 @@ This code is public domain: you are free to use, link and/or modify it in any wa
             
             HttpGet httpGet = new HttpGet(url);
             httpGet.addHeader("User-Agent",mUserAgentList.get(random_agent_num));
-            httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484" );
+            httpGet.addHeader( "Cookie","_gat=1; nsfw-click-load=off; gif-click-load=on; _ga=GA1.2.1861846600.1423061484; over18=1" );
             httpGet.setHeader("Accept","text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8");
             httpGet.setHeader("Accept-Encoding","gzip, deflate, sdch");
             httpGet.setHeader("Accept-Language", "zh-TW,zh;q=0.8,en-US;q=0.6,en;q=0.4");
